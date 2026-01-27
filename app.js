839f3d48cb24f1b7d283b895c3bf885be518ec32
ubegformercy <127954455+ubegformercy@users.noreply.github.com>
2026-01-27 17:29:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+const express = require("express");
+const path = require("path");
+const { Client, GatewayIntentBits } = require("discord.js");
+const indexRouter = require("./routes/index");
 
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+console.log("=== BoostMon app.js booted ===");
+console.log("DISCORD_TOKEN present:", Boolean(process.env.DISCORD_TOKEN));
+
+/* -------------------- */
+/* Discord Bot Setup    */
+/* -------------------- */
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`BoostMon logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+/* -------------------- */
+/* Express Web Server   */
+/* -------------------- */
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,20 +37,20 @@ app.use((req, res, next) => {
 });
 
 // Static files
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 // Main routes
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).sendFile(path.resolve(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.resolve(__dirname, "views", "404.html"));
 });
 
 // Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Internal Server Error');
+  res.status(500).send("Internal Server Error");
 });
 
 app.listen(PORT, () => {
